9a7b96b004315727b35d1f70130e631815eb5a9c
Will <willchesson@gmail.com>
2022-05-12 18:44:17 -0400

    commit tree 6bac4ce0f2a33501963199d3073251dcd0856e1d

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+require 'pry'
+
+def line(customers)
+   if customers.empty?
+    puts "The line is currently empty."
+    else 
+    current_line = "The line is currently:"
+    customers.each.with_index(1) do |name, place|
+        current_line << " #{place}. #{name}"
+        ##you can shovel into a string
+    end
+    puts current_line
+end
+end
+
+def take_a_number(customers, name)
+    customers << name;
+    puts "Welcome, #{name}. You are number #{customers.length} in line."
+end
+
+def now_serving(customers)
+    if customers.empty?
+        puts "There is nobody waiting to be served!"
+    else 
+    puts "Currently serving #{customers[0]}."
+    customers.shift
+    end
+end
+
+
